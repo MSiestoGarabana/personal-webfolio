f9ed4bd0b5fbd8ac447bd44d67e00539c9e85c92
MSiesto <miguelsiesto@gmail.com>
2023-02-21 13:35:06 +0100

    positioning MS

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
         id="header"
         class="absolute flex flex-col justify-between bg-gray-100 items-center h-screen "
       >
-        <div id="MS" class="text-5xl w-full py-3 bg-gray-200 w-full">
+        <div
+          id="MS"
+          class="text-5xl w-full py-10 bg-gray-200 w-full text-center"
+        >
           <p>MS</p>
         </div>
         <div id="Container__buttons" class="flex flex-col p-5">
