2f94ecc223fdc4de00aea2141ec4ba980c2de52e
MSiesto <miguelsiesto@gmail.com>
2023-02-21 10:59:53 +0100

    styling header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { Button } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
-    <div class="flex flex-row mb-4">
-      <header class="">
-        <div></div>
-        <div></div>
-        <div></div>
-      </header>
+    <div id="main" class="flex flex-row mb-4">
+      <div id="header" class="h-screen">
+        <div id="MS">MS</div>
+        <div id="Container__buttons" class="flex flex-col mb-4">
+          <Button>HOME</Button>
+          <Button>ABOUT</Button>
+          <Button>SKILLS</Button>
+          <Button>PROJECTS</Button>
+          <Button>CONTACT</Button>
+        </div>
+        <div id="Container__scroll">
+          <div>
+            <img alt="arrow icon"></img>
+            <p>scroll down</p>
+          </div>
+        </div>
+      </div>
       <div> main body</div>
     </div>
   );
