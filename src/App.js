d3d25dba0befed79bb5329af8b4a626f09ab9f32
MSiesto <miguelsiesto@gmail.com>
2023-02-22 11:23:33 +0100

    Creating & implementing Content.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Button } from "@mui/material";
 import SideMenu from "./SideMenu.js";
+import Content from "./Content.js";
 import "./App.css";
 
 function App() {
   return (
     <div class="h-screen w-full bg-gray-200 text-center">
       <SideMenu />
-      <div>Main</div>
-      <div>body</div>
+      <Content />
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Content = () => {
+  return (
+    <div>
+      <div>Main</div>
+      <div>body</div>
+    </div>
+  );
+};
+
+export default Content;
