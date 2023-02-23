6f25990027c9bd7da08ef89a3e7c6407b41a43b6
MSiesto <miguelsiesto@gmail.com>
2023-02-23 11:31:56 +0100

    Creating Footer & updating colors

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Button } from "@mui/material";
 import SideMenu from "./SideMenu.js";
 import Content from "./Content.js";
+import Footer from "./Footer.js";
 import "./App.css";
 
 function App() {
   return (
-    <div class="h-screen w-full bg-gray-200 text-center">
+    <div class="h-screen w-full bg-gray-100 text-center">
       <SideMenu />
       <Content />
+      <Footer />
     </div>
   );
 }
